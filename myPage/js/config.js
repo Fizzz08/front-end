34995d04351235863d313e136a55899b8128ae07
Fizan <mohammedfizan08@gmail.com>
2025-03-25 12:43:13 +0530

    smallcommit

--- myPage/js/config.js
+++ myPage/js/config.js
@@ -1,6 +1,6 @@
 // config.js - Base URL Configuration
 const BASE_URL = window.location.hostname === "localhost"
-    ? "http://localhost:3000"  // Local Development
-    : "https://chikithsa.netlify.app"; // Netlify Deployment
+    ? "http://localhost:3000/"  // Local Development
+    : "https://chikithsa.netlify.app/"; // Netlify Deployment
 
 export default BASE_URL;
